3a79103e6ccde3cbfaae031f1bbcb2c41f568bfc
Luis Felipe <luisfelipemm98@gmail.com>
2018-11-20 16:47:58 -0200

    Layout do menu quase feito e adicionando outras coisas

---
+++ Escopos/menu.php
@@ -0,0 +1,5 @@
+<nav>
+  <a href="index.php">P&aacute;gina Inicial</a>
+  <a href="#">Categorias</a>
+  <a href="#">Produtos</a>
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<title>P&aacute;gina Inicial</title>
+</head>
+  <link rel="stylesheet" type="text/css" href="Css/menu.css">
+</head>
+<body>
+  <header>
+    <?php require_once('Escopos/menu.php'); ?>
+  </header>
+  <div style="display:none;">
+  <h1>Controle de Estoque</h1>
+  <p>Aplicação utilizando PHP com PDO, MySQL e CSS simulando um controle de estoque de site comercial</p>
+  <footer>
+    <hr>
+    <h4>&copy All Rights Reserved, 2018 -- Luis Felipe</h4>
+  </footer>
+</div>
+</body>
+</html>

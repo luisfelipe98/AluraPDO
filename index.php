5de493becd8d60f34ef86b2cd7d134d7acaa0166
Luis Felipe <luisfelipemm98@gmail.com>
2018-11-28 02:14:53 -0200

    Aperfeiçoando o layout da aplicação

--- index.php
+++ index.php
@@ -2,18 +2,20 @@
 <title>P&aacute;gina Inicial</title>
 </head>
   <link rel="stylesheet" type="text/css" href="Css/menu.css">
+  <link rel="stylesheet" type="text/css" href="Css/index.css">
+  <link rel="stylesheet" type="text/css" href="Css/footer.css">
 </head>
 <body>
   <header>
     <?php require_once('Escopos/menu.php'); ?>
   </header>
-  <div style="display:none;">
-  <h1>Controle de Estoque</h1>
-  <p>Aplicação utilizando PHP com PDO, MySQL e CSS simulando um controle de estoque de site comercial</p>
-  <footer>
-    <hr>
-    <h4>&copy All Rights Reserved, 2018 -- Luis Felipe</h4>
-  </footer>
-</div>
+  <div>
+    <h1>Controle de Estoque</h1>
+    <p>Aplicação utilizando PHP com PDO, MySQL e CSS simulando um controle de estoque de site comercial</p>
+    <footer>
+      <hr>
+      <h4>&copy All Rights Reserved, 2018 -- Luis Felipe</h4>
+    </footer>
+  </div>
 </body>
 </html>

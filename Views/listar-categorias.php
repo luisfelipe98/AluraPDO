07c7304cc3d30a08cb6e4539d96dd73a6c255fdb
Luis Felipe <luisfelipemm98@gmail.com>
2018-12-26 01:26:25 -0200

    Formatando a View listar-categorias

--- Views/listar-categorias.php
+++ Views/listar-categorias.php
@@ -1,44 +1,49 @@
 <html>
 <title>P&aacute;gina Inicial</title>
 </head>
-  <link rel="stylesheet" type="text/css" href="Css/menu.css">
-  <link rel="stylesheet" type="text/css" href="Css/listar-categorias.css">
-  <link rel="stylesheet" type="text/css" href="Css/footer.css">
+  <link rel="stylesheet" type="text/css" href="../Css/menu.css">
+  <link rel="stylesheet" type="text/css" href="../Css/listar-categorias.css">
+  <link rel="stylesheet" type="text/css" href="../Css/footer.css">
 </head>
 <body>
   <header>
     <?php require_once('../Escopos/menu.php'); ?>
   </header>
+  <div>
+    <?php
+      require_once('../Banco/categoria.php');
+      $categoria = new categoria();
+      $resultado = $categoria->listar();
+    ?>
+    <h1>Categorias</h1>
+    <form action="adicionar-categoria.php">
+      <button>Adicionar Categoria</button>
+    </form>
+    <table>
+      <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Ações</th>
+      </tr>
+      <?php foreach($resultado as $linha) :?>
+        <tr>
+          <td><?php echo $linha["id"]; ?></td>
+          <td><?php echo $linha["nome"]; ?></td>
+          <td>
+            <form action="editar-categoria.php">
+              <button>Editar</button>
+            </form>
+            <form action="excluir-categoria.php">
+              <button>Excluir</button>
+            </form>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
+  <footer>
+    <hr>
+    <h4>&copy All Rights Reserved, 2018 -- Luis Felipe</h4>
+  </footer>
 </body>
 </html>
-
-<?php
-
-require_once('../Banco/categoria.php');
-
-$categoria = new categoria();
-$resultado = $categoria->listar();
-
-?>
-
-
-
-<?php
-
-
-echo "<pre>";
-print_r($resultado);
-echo "</pre>";
-
-
-
-
-
-
-
-
-
-
-
-
- ?>
